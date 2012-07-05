7ed512f1ad7726c2ec1e1655e6ef9ba5a6956da5
Jason Karns <jason@karns.name>
2012-07-04 21:25:34 -0400

    working checkpoint

--- lib/net/ssh/subshell.rb
+++ lib/net/ssh/subshell.rb
@@ -1,16 +1,27 @@
+require 'net/ssh'
 require 'telnet'
 
 module Net::SSH
   class SubShell < Telnet
     def initialize(options, &block)
-      sub_options = options.delete "SubShell"
-      super(options)
+      # rip out the subshell-specific options
+      subshell_options = options.delete "SubShell"
+
+      super(options) {}
+
+      # save original prompt, set new prompt, and begin subshell
       original_prompt = self.prompt
-      self.prompt= sub_options["prompt"]
-      self.cmd sub_options["cmd"]
-      yield block
+      self.prompt= subshell_options["Prompt"]
+      output = self.cmd subshell_options["Cmd"]
+
+      # run block in context of the sub-shell
+      yield self, output if block_given?
+
+
+      # restore prompt and exit the sub-shell
       self.prompt = original_prompt
       self.cmd "exit"
+      self
     end
   end
 end
