041cbdace1f9af2a24bde2db312f2776d0fe0a43
Jason Karns <jason@karns.name>
2012-07-04 21:34:06 -0400

    SubShell refactor

--- lib/net/ssh/subshell.rb
+++ lib/net/ssh/subshell.rb
@@ -6,34 +6,48 @@ module Net::SSH
     attr_accessor :connection, :options
     attr_reader :original_prompt
 
-    def initialize(conn, cmd, options={})
-      @connection=
-          if conn.is_a? Net::SSH::Connection::Session
-            Net::SSH::Telnet.new("Session" => conn)
-          elsif conn.is_a? Net::SSH::Telnet
-            conn
-          else
-            Net::SSH::Telnet.new(conn)
-          end
-
+    def initialize(connection, cmd=nil, options={}, &block)
+      @connection= tunnel(connection)
       @options= defaults.merge(options)
 
+      run(cmd, &block) unless cmd.nil?
+    end
+
+    def run(cmd, &block)
+      @original_prompt= @connection.prompt
+      @connection.prompt= @options[:prompt]
+      output= @connection.cmd(cmd)
       if block_given?
-        original_prompt= connection.prompt
-        @connection.prompt= @options[:prompt]
-        output= connection.cmd cmd
         yield self, output
-        @connection.prompt= original_prompt
-        @connection.cmd "exit"
+        exit
       end
+      self
     end
 
     def cmd(*args)
       @connection.cmd *args
     end
 
+    def exit
+      @connection.prompt= @original_prompt
+      @connection.cmd @options[:exit]
+    end
+
     def defaults
       {:prompt => /[$%#>] \z/n, :exit => "exit"}
+    end
+
+    private
+    def tunnel(conn)
+      if conn.is_a? Net::SSH::Connection::Session
+        Net::SSH::Telnet.new("Session" => conn)
+      elsif conn.is_a? Net::SSH::Telnet
+        conn
+      elsif conn.is_a? Net::SSH::SubShell
+        conn.connection
+      else
+        Net::SSH::Telnet.new(conn)
+      end
     end
 
   end
